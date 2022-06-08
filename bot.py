ce4f4ffde6bcf34ceb67e521773b87ffeb10cf86
HectorMG <hector980715@gmail.com>
2022-06-07 20:32:45 -0500

    Se agrega función para respuesta por defecto y función para comando /help

--- bot.py
+++ bot.py
@@ -6,6 +6,31 @@ from time import sleep
 import re
 #########################################################
 
+# Función respuesta al comando help
+
+@bot.message_handler(commands=['help'])
+def on_fallback(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    
+    response = (
+        "Estos son los comandos y órdenes disponibles:\n"
+        "\n"
+        "*/start* - Inicia la interacción con el bot\n"
+        "*/help* - Muestra este mensaje de ayuda\n"
+        "*sumar {valor1} y {valor2}* - Calcula la suma de dos valores\n"
+        "*restar {valor1} y {valor2}* - Calcula la resta de dos valores\n"
+        "*multiplicar {valor1} y {valor2}* - Calcula la multiplicación de dos valores\n"
+        "*dividir {valor1} y {valor2}* - Calcula la división de dos valores\n"
+        )
+    
+    bot.send_message(
+        message.chat.id,
+        response,
+        parse_mode="Markdown")
+
+#########################################################
+
 #Función para resolver comando /start
 @bot.message_handler(commands=['start'])
 def on_command_start(message):
@@ -110,8 +135,21 @@ def on_divide(message):
         message,
         f"\U0001F913 Resultado: {result}")
 
+#########################################################
+
+# Función respuesta por defecto
+
+@bot.message_handler(func=lambda message: True)
+def on_fallback(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    sleep(1)
+    
+    bot.reply_to(
+        message,
+        "\U0001F648 Ups, no entendí lo que me dijiste.")
 
 #########################################################
+
 if __name__ == '__main__':
     bot.polling(timeout=20)
 #########################################################
